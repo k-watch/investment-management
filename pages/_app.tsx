10e932c3f7d16159b57af0f141e2d2aea372a790
kyh <kwonyong6@gmail.com>
2022-11-18 00:04:11 +0900

    ✨[feat]: 초기 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+} from '@tanstack/react-query';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { createEmotionCache } from '@src/styles/utils/createEmotionCache';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from '@src/styles/theme';
+import { AppProps } from 'next/app';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const queryClient = new QueryClient();
+
+  return (
+    <>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+              <ReactQueryDevtools initialIsOpen={false} />
+              <CssBaseline />
+              <Component {...pageProps} />
+            </Hydrate>
+          </QueryClientProvider>
+        </ThemeProvider>
+      </CacheProvider>
+    </>
+  );
+};
+
+export default MyApp;
